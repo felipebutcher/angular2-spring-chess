d75e78cf3794709bdd2fab0f344cb6b70b815d73
Felipe Butcher <butcher@dynadrop.com>
2017-04-30 23:11:36 -0300

    King directions

--- server/src/main/java/com/dynadrop/chess/model/piece/King.java
+++ server/src/main/java/com/dynadrop/chess/model/piece/King.java
@@ -21,7 +21,10 @@ public class King implements Piece {
 
   public Direction[] getDirections() {
     ArrayList<Direction> directions = new ArrayList<Direction>();
-    //directions.add(new Direction(0,1));
+    directions.add(new Direction(0, -1, 1));
+    directions.add(new Direction(1, 0, 1));
+    directions.add(new Direction(0, 1, 1));
+    directions.add(new Direction(-1, 0, 1));
     return directions.toArray(new Direction[0]);
   }
 
