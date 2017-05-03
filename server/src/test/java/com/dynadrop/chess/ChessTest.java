b80e0b3d860e85934f4eaf1d8cad085c32cea994
Felipe Butcher <butcher@dynadrop.com>
2017-05-02 23:31:40 -0300

    refactoring test

--- server/src/test/java/com/dynadrop/chess/ChessTest.java
+++ server/src/test/java/com/dynadrop/chess/ChessTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.Before;
 import com.dynadrop.chess.model.Game;
 import com.dynadrop.chess.model.Player;
+import com.dynadrop.chess.model.Piece;
 import com.dynadrop.chess.model.piece.*;
 import com.dynadrop.chess.websocket.bean.Movement;
 import com.dynadrop.chess.websocket.bean.Position;
@@ -20,45 +21,47 @@ public class ChessTest {
 
   @Test
   public void testBoardInit() {
-    assertEquals(this.game.getBoard().getPieceAt(new Position(0,0)).getClass(), Rook.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(1,0)).getClass(), Knight.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(2,0)).getClass(), Bishop.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(3,0)).getClass(), Queen.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(4,0)).getClass(), King.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(5,0)).getClass(), Bishop.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(6,0)).getClass(), Knight.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(7,0)).getClass(), Rook.class);
+    assertEquals(this.getPieceAt(new Position(0,0)).getClass(), Rook.class);
+    assertEquals(this.getPieceAt(new Position(1,0)).getClass(), Knight.class);
+    assertEquals(this.getPieceAt(new Position(2,0)).getClass(), Bishop.class);
+    assertEquals(this.getPieceAt(new Position(3,0)).getClass(), Queen.class);
+    assertEquals(this.getPieceAt(new Position(4,0)).getClass(), King.class);
+    assertEquals(this.getPieceAt(new Position(5,0)).getClass(), Bishop.class);
+    assertEquals(this.getPieceAt(new Position(6,0)).getClass(), Knight.class);
+    assertEquals(this.getPieceAt(new Position(7,0)).getClass(), Rook.class);
     for (int i=0; i<8; i++) {
-      assertEquals(this.game.getBoard().getPieceAt(new Position(i,1)).getClass(), Pawn.class);
-      assertEquals(this.game.getBoard().getPieceAt(new Position(i,6)).getClass(), Pawn.class);
+      assertEquals(this.getPieceAt(new Position(i,1)).getClass(), Pawn.class);
+      assertEquals(this.getPieceAt(new Position(i,6)).getClass(), Pawn.class);
     }
-    assertEquals(this.game.getBoard().getPieceAt(new Position(0,7)).getClass(), Rook.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(1,7)).getClass(), Knight.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(2,7)).getClass(), Bishop.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(3,7)).getClass(), Queen.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(4,7)).getClass(), King.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(5,7)).getClass(), Bishop.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(6,7)).getClass(), Knight.class);
-    assertEquals(this.game.getBoard().getPieceAt(new Position(7,7)).getClass(), Rook.class);
+    assertEquals(this.getPieceAt(new Position(0,7)).getClass(), Rook.class);
+    assertEquals(this.getPieceAt(new Position(1,7)).getClass(), Knight.class);
+    assertEquals(this.getPieceAt(new Position(2,7)).getClass(), Bishop.class);
+    assertEquals(this.getPieceAt(new Position(3,7)).getClass(), Queen.class);
+    assertEquals(this.getPieceAt(new Position(4,7)).getClass(), King.class);
+    assertEquals(this.getPieceAt(new Position(5,7)).getClass(), Bishop.class);
+    assertEquals(this.getPieceAt(new Position(6,7)).getClass(), Knight.class);
+    assertEquals(this.getPieceAt(new Position(7,7)).getClass(), Rook.class);
   }
 
   @Test
   public void testPawnMove() throws Exception {
-    Movement movement = new Movement(new Position(0,6), new Position(0,4));
-    assertEquals(true, this.game.movePiece(movement));
-    assertEquals(this.game.getBoard().getPieceAt(new Position(0,4)).getClass(), Pawn.class);
-    //TODO must move the other player as well
-    //movement = new Movement(new Position(7,6), new Position(7,4));
-    //assertEquals(true, this.game.movePiece(movement));
-    //movement = new Movement(new Position(1,6), new Position(1,5));
-    //assertEquals(true, this.game.movePiece(movement));
-    //movement = new Movement(new Position(2,6), new Position(3,5));
-    //assertEquals(false, this.game.movePiece(movement));
+    this.testMovePiece(new Movement(new Position(0,6), new Position(0,4)), true, Pawn.class);
+    this.testMovePiece(new Movement(new Position(7,1), new Position(7,3)), true, Pawn.class);
+    this.testMovePiece(new Movement(new Position(7,6), new Position(7,4)), true, Pawn.class);
+    this.testMovePiece(new Movement(new Position(6,1), new Position(6,3)), true, Pawn.class);
+    this.testMovePiece(new Movement(new Position(1,6), new Position(1,5)), true, Pawn.class);
+    this.testMovePiece(new Movement(new Position(5,1), new Position(5,3)), true, Pawn.class);
+    this.testMovePiece(new Movement(new Position(2,6), new Position(3,5)), false, Pawn.class);
   }
 
-  @Test
-  public void testBlackPawnMove() throws Exception {
-    //TODO Movement movement = new Movement(new Position(0,1), new Position(0,3));
-    //TODO assertEquals(true, this.game.movePiece(movement));
+  private void testMovePiece(Movement movement, boolean expected, Class classObj) throws Exception {
+    assertEquals(expected, this.game.movePiece(movement));
+    if (this.getPieceAt(movement.getPosition2()) != null) {
+      assertEquals(this.getPieceAt(movement.getPosition2()).getClass(), classObj);
+    }
+  }
+
+  private Piece getPieceAt(Position position) {
+    return this.game.getBoard().getPieceAt(position);
   }
 }
