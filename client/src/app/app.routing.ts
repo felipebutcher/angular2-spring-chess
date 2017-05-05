cbc3a40ce9fe1ad93de55e6eb2b875a77f603453
Felipe Butcher <butcher@dynadrop.com>
2017-05-04 22:18:45 -0300

    useHash for routing on server

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -19,4 +19,4 @@ const appRoutes : Routes = [
   }*/
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true });
