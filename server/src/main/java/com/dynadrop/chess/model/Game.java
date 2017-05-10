947b378c41b084e4e40688168ed38f07e066d615
Felipe Butcher <butcher@dynadrop.com>
2017-05-09 22:21:15 -0300

    forgot ;

--- server/src/main/java/com/dynadrop/chess/model/Game.java
+++ server/src/main/java/com/dynadrop/chess/model/Game.java
@@ -137,7 +137,7 @@ public class Game {
   }
 
   private void doCastling(Movement movement) {
-    System.out.println("castling done")
+    System.out.println("castling done");
     int y = movement.getPosition2().getY();
     if (movement.getPosition1().getX() == movement.getPosition2().getX() + 2) {
       //castling left
